32080b475efc81b9f94cc553cdb4ca189258d565
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2023-10-16 19:49:02 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -46,7 +46,7 @@ class Settings(BaseSettings):
         db = info.data.get("POSTGRES_DB")
 
         if all([user, password, host, db]):
-            return f"postgres://{user}:{password}@{host}/{db}?sslmode=require"
+            return f"postgresql://{user}:{password}@{host}/{db}?sslmode=require"
         else:
             return None
 
