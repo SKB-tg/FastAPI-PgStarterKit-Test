208b7bbd3b46ad2d828f6f6c1d0dfa269ceb02c2
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2023-10-16 12:25:13 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    SERVER_HOST: https://fastapi-pgstarterkit-test.onrender.com
+    SERVER_HOST: str = "https://fastapi-pgstarterkit-test.onrender.com"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
