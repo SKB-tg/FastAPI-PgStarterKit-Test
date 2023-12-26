97ce1b8370aee92ceba508a19c92fcb161907de0
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2023-12-26 13:23:20 +0300

    Add files via upload

---
+++ app/schemas/vakancy.py
@@ -0,0 +1,48 @@
+from typing import Optional, Date
+
+from pydantic import ConfigDict, BaseModel
+
+
+# Shared properties
+class VakancyBase(BaseModel):
+    id_vakancy: int = 1
+    name: str = "--"
+    kategory: str = "--"
+    link: str = "--"
+    company: str = "--"
+    price: int = 100
+    description_short: str = "--"
+    description_full: str = "--"
+    date_publikate: Date = None
+
+# Properties to receive on item creation
+class VakancyCreate(VakancyBase):
+    pass
+
+
+# Properties to receive on item update
+class VakancyUpdate(VakancyBase):
+    pass
+
+
+# Properties shared by models stored in DB
+class VakancyInDBBase(VakancyBase):
+    id_vakancy: int
+    name: str
+    kategory: str
+    link: str
+    company: str
+    price: int
+    description_short: str
+    description_full: str 
+    date_publikate: Date 
+
+
+# Properties to return to client
+class Vakancy(VakancyInDBBase):
+    pass
+
+
+# Properties properties stored in DB
+class VakancyInDB(VakancyInDBBase):
+    pass
