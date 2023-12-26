c38635b3750e6716ffec3094fb5e87cfb5f163ee
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2023-12-27 00:06:32 +0300

    Add files via upload

---
+++ app/api/api_v1/endpoints/services.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends
+#from pydantic.networks import EmailStr
+
+from app import models, schemas
+from app.api import deps
+
+from app.service import parse_data_vacancy, ParserData
+
+router = APIRouter()
+
+
+@router.post("/quere-new-vakamcy/", response_model=schemas.Msg, status_code=201)
+def quere_new_vakamcy(
+    data: ParserData,
+    current_user: models.User = Depends(deps.get_current_active_superuser),
+) -> Any:
+    """
+    Test emails.
+    """
+    await parse_data_vacancy(data=data)
+    return {"msg": "Ok"}
+
+@router.get("/{info}")
+def get_new_vakamcy(
+    info: str,
+    current_user: models.User = Depends(deps.get_current_active_superuser),
+) -> Any:
+    """
+    info=chat_id&bot_token.
+    """
+    data: dict = {"chat_id": info.replace('&')[0], "bot_token": info.replace('&')[1]}
+    await parse_data_vacancy(data=data)
+    return {"msg": "Ok"}
