9aa48336cfbe7fbee08970518938cea8d8cf2bd1
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2023-12-27 00:09:45 +0300

    Add files via upload

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import items, login, users, utils, vakancies
+from app.api.api_v1.endpoints import items, login, users, utils, vakancies, services
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -8,5 +8,5 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(vakancies.router, prefix="/vakancies", tags=["vakancy"])
-api_router.include_router(items.router, prefix="/service", tags=["quere-new-vakamcy"])
+api_router.include_router(services.router, prefix="/services", tags=["quere-new-vakamcy"])
 
