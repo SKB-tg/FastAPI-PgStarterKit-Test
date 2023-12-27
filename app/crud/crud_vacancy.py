9c9054e2bbe93a7a6f2e1e6345eb754099d29698
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2023-12-27 10:22:03 +0300

    Add files via upload

--- app/crud/crud_vacancy.py
+++ app/crud/crud_vacancy.py
@@ -10,10 +10,10 @@ from app.schemas.vakancy import VakancyCreate, VakancyUpdate
 
 class CRUDVakancy(CRUDBase[Vakancy, VakancyCreate, VakancyUpdate]):
     def create_with_owner(
-        self, db: Session, *, obj_in: VakancyCreate, owner_id: int
+        self, db: Session, *, obj_in: VakancyCreate #, owner_id: int
     ) -> Vakancy:
         obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data, owner_id=owner_id)
+        db_obj = self.model(**obj_in_data) #, owner_id=owner_id)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
