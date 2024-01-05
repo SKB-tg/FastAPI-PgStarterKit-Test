fde589d20875fe58978735871f05370133eca2d7
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-05 15:27:08 +0300

    Update api.py

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -4,12 +4,6 @@ from typing import Any
 from app.api.api_v1.endpoints import items, login, users, utils, vakancies, services
 
 api_router = APIRouter()
-status_r = APIRouter()
-api_router.include_router(status_r)
-
-@status_r.get("/status", status_code=200)
-def get_status_r():
-    return {"msg": "Ok"}
     
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
