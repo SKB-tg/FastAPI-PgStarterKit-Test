b66db1fcaa01d6f9a92cbbce331060efb762654d
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-06 08:11:17 +0300

    Add files via upload

--- app/api/api_v1/endpoints/services.py
+++ app/api/api_v1/endpoints/services.py
@@ -11,16 +11,16 @@ from app.service import parse_data_vacancy, ParserData
 router = APIRouter()
 
 
-@router.post("/quere-new-vakamcy/", response_model=schemas.Msg, status_code=201)
+@router.post("/quere-new-vakamcy/", response_model=schemas.Vakancy, status_code=201)
 def quere_new_vakamcy(
-    data: ParserData,
+    data: dict,
     current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
     """
     Test emails.
     """
-    parse_data_vacancy(data=data, owner_id=current_user.id)
-    return {"msg": "Ok"}
+    data_out = parse_data_vacancy(data=data, owner_id=current_user.id)
+    return {"vakancy": data_out}
 
 @router.get("/{info}")
 def get_new_vakamcy(
