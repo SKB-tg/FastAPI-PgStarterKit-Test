2a4e7b3ed2f16f7505214b4dadd4801fe74d2580
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-09 18:21:45 +0300

    Update service.py

--- app/service.py
+++ app/service.py
@@ -54,7 +54,7 @@ def parse_data_vacancy(owner_id: int, data: Optional[ParserData] = parser_data_d
 	# ]
 	# pag = 1 # from setting import pag колич.сканируемых страниц
 	# fd = 3 # дней назад
-	item_in = schemas.vakancy.VakancyCreate()
+	item_in = schemas.vakancy.VakancyCreate
 	parser = MyUniParser(chat_id=data.chat_id, bot_token=data.bot_token)
 	html = parser.get_requests_html(data.url)
 	list_vacancy = parser.parse_data(html, data.kategory, data.page, data.fd,
