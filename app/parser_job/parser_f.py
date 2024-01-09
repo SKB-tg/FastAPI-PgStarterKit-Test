e34877b9344fb723e0c9ff42606423a0ae41100f
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-09 21:35:05 +0300

    Update parser_f.py

--- app/parser_job/parser_f.py
+++ app/parser_job/parser_f.py
@@ -75,9 +75,9 @@ class MyUniParser:
         # session = HTMLSession()
         print(url)
         if str(url)[8] == "r":
-            url1 = url[:17] + ':' + port + url[17:url.find('?')]
+            url1 = url[:17] + ':' + port + url[17:url.find('?')+1]
         else:
-            url1 = url[:13] + ':' + port + url[13:url.find('?')]
+            url1 = url[:13] + ':' + port + url[13:url.find('?')+1]
         try:
             # Send an HTTP request to the provided URL
             headers = {
