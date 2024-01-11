f1482cf6ed5a176234be533d29333b48e016d59d
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-11 14:24:39 +0300

    Update vakancies.py

--- app/api/api_v1/endpoints/vakancies.py
+++ app/api/api_v1/endpoints/vakancies.py
@@ -35,7 +35,7 @@ def read_items(
         items = crud.vakancy.get_multi_by_owner(
             db=db, owner_id=current_user.id, skip=skip, limit=limit
         )
-    print(items.__dict__)
+    print(items[0].__dict__)
     return items
 
 
