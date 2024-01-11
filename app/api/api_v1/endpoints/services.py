e264c6d4c14582dcae7149850e0717823b0ad56a
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-12 01:13:57 +0300

    Update services.py

--- app/api/api_v1/endpoints/services.py
+++ app/api/api_v1/endpoints/services.py
@@ -13,7 +13,7 @@ from app.service import parse_data_vacancy, ParserData
 router = APIRouter()
 
 
-@router.post("/quere-new-vakamcy/", response_model=PlainTextResponse)  #chemas.Vakancy)
+@router.post("/quere-new-vakamcy/", response_class=PlainTextResponse)  #chemas.Vakancy)
 def quere_new_vakamcy(
     data: dict,
     db: Session = Depends(deps.get_db),
