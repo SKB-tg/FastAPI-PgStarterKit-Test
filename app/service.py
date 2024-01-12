6e6fc3a146c19f588805c9a0f987a80250ce0df7
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-12 21:39:28 +0300

    Update service.py

--- app/service.py
+++ app/service.py
@@ -78,6 +78,7 @@ def parse_data_vacancy(db: Session, owner_id: int, data: ParserData = parser_dat
 			print(77, resu.price)
 			item_out = crud.vakancy.convert_schemas_to_model(item_in)
 			item_out.link = "--"
+			item_out.description_full = "--"
 			return item_out
 		else:
 			res = crud.vakancy.create_with_owner(db, obj_in=item_in)#, owner_id=owner_id)
