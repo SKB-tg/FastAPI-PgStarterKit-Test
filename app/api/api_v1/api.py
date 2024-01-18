ab33774d6fd6c44d87ac394243c0f3e8929646e1
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-18 22:49:56 +0300

    Update api.py

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -9,6 +9,6 @@ api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(vakancies.router, prefix="/vakancies", tags=["vakancy"])
+api_router.include_router(vakancies.router, prefix="/vakancies", tags=["vakancies"])
 api_router.include_router(services.router, prefix="/services", tags=["services"])
 
