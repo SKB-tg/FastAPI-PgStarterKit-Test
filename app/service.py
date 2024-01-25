1b26ed587c787a76ea9a728f0171bbf56fd16128
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-25 09:11:11 +0300

    Update service.py

--- app/service.py
+++ app/service.py
@@ -75,11 +75,10 @@ def parse_data_vacancy(db: Session, owner_id: int, data: ParserData ):# -> schem
 		item_in.date_publikate = v['Дата размещения']
 		resu = crud.vakancy.get_id_vakancy(db, item_in.id_vakancy)
 		print(76,item_in)
-		print(78, resu.__dict__)
 
 		if resu:
 			#resul = crud.vakancy.update(db, db_obj=resu, obj_in=item_in)
-			#print(77, resu.price)
+			print(77, resu.__dict__)
 			resu.description_full = "--"
 			item_out = crud.vakancy.convert_schemas_to_model(item_in)
 			return resu
