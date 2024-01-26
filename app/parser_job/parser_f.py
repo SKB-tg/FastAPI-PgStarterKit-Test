4a3fead7c88bf73cdd97b4fae95c692779653e85
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-26 19:07:30 +0300

    Update parser_f.py

--- app/parser_job/parser_f.py
+++ app/parser_job/parser_f.py
@@ -182,7 +182,7 @@ class MyUniParser:
         url = f"https://api.telegram.org/bot{token}/sendmessage"
         payload = {"chat_id": chat_id, "text": message_text}
         response = requests.post(url, data=payload)
-        print(response.json["result"][0])
+        print(response.json()["result"][0])
         # Проверяем статус код ответа и возвращаем результат
         if response.status_code == 200:
             return True
