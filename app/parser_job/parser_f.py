bd1a79a42bae4223eb59eb896f01ec935cad1e13
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-28 12:26:24 +0300

    Update parser_f.py

--- app/parser_job/parser_f.py
+++ app/parser_job/parser_f.py
@@ -179,7 +179,7 @@ class MyUniParser:
         #Проверяем на дублирование
         res = crud.vakancy.get_col(db, message_dict["ID вакансии"])
         if res:
-            print(177, dict(**res)['id_vakancy'])    
+            print(177, res.__dict__)    
             return False
         # Формируем текст сообщения из словаря
         message_text = "\n".join([f"{key}:\n {value}\n" for key, value in message_dict.items()])
