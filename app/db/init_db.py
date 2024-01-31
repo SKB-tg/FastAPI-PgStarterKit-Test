3ce94d63a2b5c42a04035ec8f5bf0d034ce841d0
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-31 19:43:53 +0300

    Update init_db.py

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -13,8 +13,8 @@ def init_db(db: Session) -> None:
     # Tables should be created with Alembic migrations
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
-    #base.Base.metadata.create_all(bind=engine)
-    base.Vakancy.metadata.drop_all(bind=engine)
+    base.Base.metadata.create_all(bind=engine)
+    #base.Vakancy.metadata.drop_all(bind=engine)
 
 
     user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
