fbc011f175a89aad6cc53e0c113986d86df593ce
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-31 19:24:47 +0300

    Update init_db.py

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -14,7 +14,7 @@ def init_db(db: Session) -> None:
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
     #base.Base.metadata.create_all(bind=engine)
-    base.Base.vakancy.metadata.drop_all(bind=engine)
+    base.Base.metadata.drop_all(bind=engine, tables=["vakancy",])
 
 
     user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
