40b748ab80d7ebdd447e3c664c8d16c4703648a9
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-02-26 20:05:07 +0300

    Update parser_f.py

--- app/parser_job/parser_f.py
+++ app/parser_job/parser_f.py
@@ -192,7 +192,7 @@ class MyUniParser:
     def send_message_to_telegram(self, chat_id, token, message_dict):
         #message_dict.pop('link_vakancy')
         message_dict.pop("№")
-        message_dict.pop("message_id")
+        message_dict.pop("mess_id")
         message_dict["категории"] = f"#{message_dict['категории']}"
         #Проверяем на дублирование
 
